eb360ae71c1ae7695c02acb2d4b2891b31d1d00a
zizi-shoot <ol.polyanskiy@gmail.com>
2020-06-16 22:32:46 +0300

    add script to increase cart amount

--- js/script.js
+++ js/script.js
@@ -2,6 +2,15 @@
 
 document.querySelector('.header__burger').addEventListener('click', openMenu);
 document.querySelector('.menu .header__burger').addEventListener('click', closeMenu);
+document.querySelector('.stock__item').addEventListener('click', increaseCart)
+
+function increaseCart() {
+	let cartAmount = +document.querySelector('.cart__link').getAttribute('data-amount');
+	cartAmount++;
+	document.querySelector('.cart__link').setAttribute('data-amount', cartAmount);
+}
+
+
 
 function openMenu() {
 	document.querySelector('.menu').style.bottom = 0;
@@ -13,5 +22,6 @@ function closeMenu() {
 	document.querySelector('.menu').style.bottom = '100vh';
 	document.querySelector('.menu__nav').style.right = '100vw';
 	document.querySelector('.menu__footer').style.top = '200vh';
-	
 }
+
+
